7105bdd1a4b4329800e89480d52702914cf542a3
Thibaud JEAY <thibaud.jeay@gmail.com>
2026-03-29 16:37:33 +0200

    fix: supprimer export metadata depuis composant client studio

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -3,8 +3,6 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '@/sanity.config'
 
-export { metadata, viewport } from 'next-sanity/studio'
-
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
